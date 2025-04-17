e1705300e371c2aaa2bf4b59a0f4af5426cb2470
sbk554 <sbk8731@naver.com>
2025-04-17 16:27:55 +0900

    first commit

--- src/components/Poket.jsx
+++ src/components/Poket.jsx
@@ -1,11 +1,24 @@
 import '../css/Poket.css';
+import loading from '../images/loading.gif'
 
 export default function Poket({renderPokemonList, isLoading, hasMore}) {
   return (
     <div className="poket-container">
       {renderPokemonList()}
-      {isLoading && <p style={{ textAlign: 'center' }}>불러오는 중...</p>}
-      {!hasMore && <p style={{ textAlign: 'center' }}>모든 포켓몬을 불러왔습니다!</p>}
+      {isLoading && 
+          <div style={{ textAlign: 'center', display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
+              <img
+                src={loading}
+                style={{ width: '60px', height: '60px', marginRight: '8px' }}
+              />
+              <p style={{ margin: 0 ,fontSize:'30px', fontWeight:'700'}}>불러오는 중...</p>
+          </div>
+        }
+      {!hasMore && 
+        <div style={{ textAlign: 'center', display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
+          <p style={{ margin: 0 ,fontSize:'30px', fontWeight:'700' }}>모든 포켓몬을 불러왔습니다!</p>
+        </div>
+      }
     </div>
   );
 }
