9180c6524e78bbdcaac702e12ef7d6ebe54980cc
sbk554 <sbk8731@naver.com>
2025-04-18 14:02:08 +0900

    zzzz

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -1,4 +1,5 @@
 import React, { useEffect, useRef } from 'react';
+import '../css/Modal.css';
 
 export default function Modal({ isOpen, onClose, content }) {
   const dialogRef = useRef();
@@ -17,11 +18,28 @@ export default function Modal({ isOpen, onClose, content }) {
         <div className="dialog-container">
             <img src={content?.image} />
             <div className="dialog-container-wrap">
-                <div>
-                    <span>{content?.text || "modal text"}</span>
-                    <h3>{content?.title || "This is modal"}</h3>
+                <div className="wrap-title">
+                    <h3>
+                        <span>{content?.text || "modal text"}</span>
+                        {content?.title || "This is modal"}
+                    </h3>
                 </div>
                 <div>
+                    <div className="wrap-content">
+                        <div>
+                            <span>
+                                타입
+                                <p>{content?.types}</p>
+                            </span>
+                            <span>키</span>
+                            <span>분류</span>
+                        </div>
+                        <div>
+                            <span>성별</span>
+                            <span>몸무게</span>
+                            <span>특성</span>
+                        </div>
+                    </div>
                     <form method="dialog">
                         <button onClick={onClose}>닫기</button>
                     </form>
